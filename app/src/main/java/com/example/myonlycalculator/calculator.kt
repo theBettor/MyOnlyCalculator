6d08c2feb25b3589b429cf7d1da8e7137bae4275
theBettor <be0128st@gmail.com>
2024-03-07 20:19:18 +0900

    Lv1 완성

--- app/src/main/java/com/example/myonlycalculator/calculator.kt
+++ app/src/main/java/com/example/myonlycalculator/calculator.kt
@@ -1,13 +1,41 @@
 package com.example.myonlycalculator
 
 fun main() {
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
 
+    var calc = Calculator()
+
+    var addresult = calc.add(a, b)
+    var subresult = calc.sub(a, b)
+    var multiplyresult = calc.multiply(a, b)
+    var divideresult = calc.divide(a, b)
+
+    println("정수 덧셈결과: ${addresult}")
+    println("정수 뺄셈결과: ${subresult}")
+    println("정수 곱셈결과: ${multiplyresult}")
+    println("정수 나눗셈결과: ${divideresult}")
 }
 
 class Calculator {
-    var addition:String = ""
-    var hairColor:String = ""
-    var height:Double = 0.0
-    var age:Int = 0
-    var gender:String = ""
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+
+    }
+    fun sub(a: Int, b: Int): Int {
+        return a - b
+    }
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+    fun divide(a: Int, b: Int): Int {
+        return a
+    }
 }
+
+
+
+
+
+
