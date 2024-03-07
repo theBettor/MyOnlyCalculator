55aa6fe68f3254c69993e72c3484c7c2e2fe3fcb
theBettor <be0128st@gmail.com>
2024-03-07 23:50:43 +0900

    깃 테스트

--- app/src/main/java/com/example/myonlycalculator/calculator.kt
+++ app/src/main/java/com/example/myonlycalculator/calculator.kt
@@ -6,13 +6,13 @@ fun main() {
 
     var calc = Calculator()
 
-    var addresult = calc.add(a, b)
+    var baddresult = calc.add(a, b)
     var subresult = calc.sub(a, b)
     var multiplyresult = calc.multiply(a, b)
     var divideresult = calc.divide(a, b)
     var remainderresult = calc.remainder(a, b)
 
-    println("정수 덧셈결과: ${addresult}")
+    println("정수 덧셈결과: ${baddresult}")
     println("정수 뺄셈결과: ${subresult}")
     println("정수 곱셈결과: ${multiplyresult}")
     println("정수 나눗셈결과: ${divideresult}")
